e414f886d0e56fb4b721adf3018581d85d707380
Dave Curry <lordsnoosh@gmail.com>
2021-03-01 17:36:16 -0800

    working game!

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 /*----- constants -----*/
-const lookup = {'null': '0',
-'1': 'pink',
-'-1': 'grey',
+
+const lookup = {'null': null,
+'1': 'url("../tick-tack-toe/images/blue-gorilla.png")',
+'-1': 'url("../tick-tack-toe/images/red-rhino.png")',
 };
     
     //I really want to figure out how to add-
   // a unique symbol to populate when a box is selected...
   
-  const gorToken = src ="../tick-tack-toe/images/blue-gorilla.png";
-  const rhiToken = src ="../tick-tack-toe/images/red-rhino.png";
+  // const gorToken = src ="../tick-tack-toe/images/blue-gorilla.png";
+  // const rhiToken = src ="../tick-tack-toe/images/red-rhino.png";
   
 const winningComb = [
   [0, 1, 2],[3, 4, 5],
@@ -27,26 +28,11 @@ const display = document.querySelector('h1');
 
 /*----- event listeners -----*/
 document.querySelector('table').addEventListener('click', handleMove);
-//  document.querySelector('table').addEventListener('click', tokenSet);
 document.querySelector('button').addEventListener('click', initialize);
 
 /*----- functions -----*/
 
 initialize();
-
-//  function tokenSet() {
-// //  console.log('heck yeah')
-// gametable.forEach(function(box, idx) {
-//   boxes[idx].style.src = gorToken;
-// });
-// // if (winner === 'T') {
-// //   display.innerHTML = 'CATS GAME';
-// // } else if (winner) {
-// //   display.innerHTML = `Well done ${lookup[winner].toLowerCase()}!`;
-// // } else {
-// //   display.innerHTML = `It's ${lookup[turn].toLowerCase()}'s turn`;
-// // }
-//  }
 
 function handleMove(evt) {
   const idx = parseInt(evt.target.id.replace('box', ''));
@@ -70,17 +56,19 @@ function getWinner() {
 
 function render() {
   gametable.forEach(function(box, idx) {
-    boxes[idx].style.background = lookup[box];
+    gametable.forEach(function(box, idx) {
+     boxes[idx].style.background = lookup[box];
+  });
   });
   if (winner === 'T') {
     display.innerHTML = 'CATS GAME';
   } else if (winner) {
-    display.innerHTML = `Well done ${lookup[winner].toLowerCase()}!`;
+    display.innerHTML = `Well done Player ${turn > 0 ? 1 : 2}!`;
   } else {
-    display.innerHTML = `It's ${lookup[turn].toLowerCase()}'s turn`;
+    display.innerHTML = `It's Player ${turn > 0 ? 1 : 2}'s turn`;
   }
 }
-
+// ${lookup[turn].toLowerCase()}
 function initialize() {
   gametable = [null,null,null,null,null,null,null,null,null];
   turn = 1;
